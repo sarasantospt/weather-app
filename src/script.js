835a7a07c8f50d17a97bf46ebf16a85015f97412
sarasantospt <sarasantos95pt@gmail.com>
2023-05-23 14:15:35 +0100

    added icon element

--- src/script.js
+++ src/script.js
@@ -36,6 +36,14 @@ function showWeatherForecast(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
